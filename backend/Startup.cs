c6ce0e3f12c9f0ef6e8c830bec006a0439024bc5
Johnny Cheng <cheng306@uw.edu>
2017-11-28 17:36:07 -0800

    add Message Model, add MessageController Controller

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -23,6 +23,13 @@ namespace backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddPolicy("Cors", builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            }));
             services.AddMvc();
         }
 
@@ -33,7 +40,7 @@ namespace backend
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors("Cors");
             app.UseMvc();
         }
     }
