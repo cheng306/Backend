225c10ec73a5aebf62d3f64d8ba1a1e486ab1ad5
cheng306 <cheng306@uw.edu>
2017-12-04 16:45:51 -0800

    Create a User Model. create  a authenticateCotroller that would return token in post action.

---
+++ backend/Controllers/AuthenticateController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Authenticate")]
+    public class AuthenticateController : Controller
+    {
+        [HttpPost("register")]
+        public JwtPacket Register([FromBody] User user)
+        {
+            var jwt = new JwtSecurityToken();
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return new JwtPacket() { Token = encodedJwt };
+        }
+    }
+}
